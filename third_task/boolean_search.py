e60fa0b49e5b3433181d4ad6500532329d8ad2c6
Niyaz Khadimullin <niyaz.hadimullin@emergingtravel.com>
2024-02-21 20:09:54 +0300

    added extra fiels

--- third_task/boolean_search.py
+++ third_task/boolean_search.py
@@ -69,6 +69,7 @@ if __name__ == "__main__":
         "~технический & войти",
         "технический & ~войти",
         "технический | ~войти",
+        "(технический | ~войти) & поддержка & количество"
     ]
     for string in search_strings:
         print(boolean_search.searh(string))
